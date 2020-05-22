b84d1aa5e038bb9f6844785dcde55dec06fbfd32
alejandrovillagomez <alejandrovillago@gmail.com>
2020-05-22 10:35:48 -0500

    A.1 c) adding a commons.lang dependency for generating effective hash functions

---
+++ jmjrst.main/src/test/java/plugins/PluginForJmjrstTest.java
@@ -0,0 +1,33 @@
+package plugins;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PluginForJmjrstTest {
+
+    PluginForJmjrst plugin1;
+    PluginForJmjrst plugin2;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void compareTo() {
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+}
