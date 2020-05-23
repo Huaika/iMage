d5a3c22d168c000418ea9e08fa8a71e8ab183849
alejandrovillagomez <alejandrovillago@gmail.com>
2020-05-22 19:28:42 -0500

    A.2 b) adding the iMage.JavaCrashCourse-plugin into the modules of the pom from iMage A.2 c) initializing the immutable list of Strings with the java versions A.2 d) implementing the configuration

--- iMage.JavaCrashCourse-plugin/src/main/java/JavaCrashCourse.java
+++ iMage.JavaCrashCourse-plugin/src/main/java/JavaCrashCourse.java
@@ -1,6 +1,10 @@
 import org.jis.Main;
 import plugins.PluginForJmjrst;
 
+import javax.swing.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * This class serves as a prototype for a sample plugin.
  *
@@ -9,17 +13,30 @@ import plugins.PluginForJmjrst;
  */
 public class JavaCrashCourse extends PluginForJmjrst {
 
+    /**
+     * the main class in which the plugin will operate
+     */
+    private Main main;
+
+    /**
+     * a list of all java releases to this date
+     */
+    private List javaReleases;
 
     public String getName() {
         return this.getClass().getName();
     }
 
     public int getNumberOfParameters() {
-        return 0;
+        return javaReleases.size();
     }
 
     public void init(Main main) {
-
+        this.main = main;
+        javaReleases = List.of(
+                "Java SE 9", "Java SE 10", "Java SE 11",
+                "Java SE 12", "Java SE 13", "Java SE 14"
+        );
     }
 
     public void run() {
@@ -31,6 +48,8 @@ public class JavaCrashCourse extends PluginForJmjrst {
     }
 
     public void configure() {
-
+        JOptionPane.showMessageDialog(
+                this.main,javaReleases.stream().collect(Collectors.joining("\n"))
+        );
     }
 }
