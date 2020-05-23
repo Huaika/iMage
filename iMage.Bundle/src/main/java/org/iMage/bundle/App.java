74e1d0f1acc2c420a73d50d8ea7c2dfe32907839
alejandrovillagomez <alejandrovillago@gmail.com>
2020-05-23 06:31:14 -0500

    A.3 c) adding the app class with a main method

---
+++ iMage.Bundle/src/main/java/org/iMage/bundle/App.java
@@ -0,0 +1,17 @@
+package org.iMage.bundle;
+
+/**
+ *
+ * @author alejandrovillagómez
+ * @version 1.0
+ */
+public class App {
+
+    /**
+     * the main method
+     * @param args the arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
