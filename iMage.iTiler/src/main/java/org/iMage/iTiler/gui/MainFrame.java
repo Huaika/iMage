c8d54d9464f46e14963498c53d1c4a50dd64bf33
alejandrovillagomez <alejandrovillago@gmail.com>
2020-06-06 18:47:34 -0500

    A.1 a) extending the implementation of main frame

--- iMage.iTiler/src/main/java/org/iMage/iTiler/gui/MainFrame.java
+++ iMage.iTiler/src/main/java/org/iMage/iTiler/gui/MainFrame.java
@@ -1,8 +1,38 @@
 package org.iMage.iTiler.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class MainFrame {
+public class MainFrame extends JFrame {
+
+    /**
+     * the toolbar which contains the run and rectangle Button
+     */
+    private ToolBar toolBar;
+
+    public MainFrame() {
+        super("iTiler");
+
+        //setup the Layout-Manager
+        setLayout(new BorderLayout());
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(850, 450));
+        setResizable(false);  // makes the frame no more resizeable
+
+        //initiating the components
+        toolBar = new ToolBar();
+
+        //adding the components into the frame
+        add(toolBar, BorderLayout.SOUTH);
+
+
+        //Display the window
+        pack();
+        setVisible(true);
+
+    }
+
 
 
 }
