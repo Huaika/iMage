5da5ecac5578f85b0c54df51a21fb8f7d15c8908
alejandrovillagomez <alejandrovillago@gmail.com>
2020-06-06 18:46:37 -0500

    A.1 a) defining macro structure of the lower ToolBar

---
+++ iMage.iTiler/src/main/java/org/iMage/iTiler/gui/ToolBar.java
@@ -0,0 +1,59 @@
+package org.iMage.iTiler.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ToolBar extends JPanel {
+
+
+    private JButton runButton;
+    private JComboBox figureBox;
+    private JButton showTilesButton;
+    private JButton loadTilesButton;
+    private JTextField widthField;
+    private JTextField heightField;
+    private JLabel tileSizeLabel;
+    private JLabel artistLabel;
+
+
+    public ToolBar() {
+
+        //specifying the border
+        setBorder(BorderFactory.createTitledBorder("Artistic"));
+
+        //initiating the Components
+        runButton = new JButton("run");
+        figureBox = new JComboBox();
+        showTilesButton = new JButton("Show Tiles");
+        loadTilesButton = new JButton("Load Tiles");
+        widthField = new JTextField(4);
+        heightField = new JTextField(4);
+        tileSizeLabel = new JLabel("Tile Size");
+        artistLabel = new JLabel("Artist");
+
+        //setting out the layout
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        //Setup Combo Box
+        DefaultComboBoxModel figureModel = new DefaultComboBoxModel();
+        figureModel.addElement("rectangle");
+        figureModel.addElement("triangle");
+        figureBox.setModel(figureModel);
+        figureBox.setSelectedIndex(0);
+
+        //adding the components to the toolbar
+        add(tileSizeLabel);
+        add(widthField);
+        add(new JLabel("X"));
+        add(heightField);
+        add(loadTilesButton);
+        add(showTilesButton);
+        add(artistLabel);
+        add(figureBox);
+        add(runButton);
+
+
+
+    }
+
+}
